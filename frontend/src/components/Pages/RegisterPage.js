096bb9d2d826404d2355eb2691345723fa2aa10f
Krish Patel <krishrpatel@gmail.com>
2025-02-18 13:02:20 -0500

    Small ui fixes

--- frontend/src/components/Pages/RegisterPage.js
+++ frontend/src/components/Pages/RegisterPage.js
@@ -1,12 +1,11 @@
 import React from "react";
 import GoogleSignIn from "../Shared_components/Login_button";
 import styles from "./RegisterPage.module.css"; // Import CSS module
-
 function RegisterPage() {
   return (
     <div className={styles.registerAppContainer}>
       <div className={styles.registerCard}>
-        <h2 className={styles.registerTitle}>Welcome to Our Platform</h2>
+        <h2 className={styles.registerTitle}>Welcome to RoomEase</h2>
         <p className={styles.registerDescription}>Please sign in with your Google account to continue.</p>
         <GoogleSignIn />
       </div>
